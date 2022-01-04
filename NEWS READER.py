58f98c570f9237aa70b6f918d639200c08dada60
Shravan <96918109+shravan2006@users.noreply.github.com>
2022-01-04 21:32:58 +0530

    Update NEWS READER.py

--- NEWS READER.py
+++ NEWS READER.py
@@ -10,12 +10,12 @@ def headlines():
        'apiKey=3dfea29fe4ac4114b23c82f3b3ad75be')
        alldata = data.json()
        articles = alldata['articles']
-       for i in range(1,2):
+       for i in range(1,11):
               a = articles[i]['description']
               print(a)
               speak(f'news number{i} . {a}')
 
-speak("Hello .  welcome to SK news centre ")
+speak("Hello .  welcome to news1 news centre ")
 speak("here are the headlines ")
 headlines()
 speak(" . thank you for listening")
